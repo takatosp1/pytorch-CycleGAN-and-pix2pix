2e4a4b0bc89307c03e6d09999a7d3d697aa63418
Yi Shi <yishi.1204@gmail.com>
2018-07-31 23:22:31 -0700

    script change

--- train_pix2pix_gt_mask.sh
+++ train_pix2pix_gt_mask.sh
@@ -12,4 +12,4 @@ then
   GATE_ON_B=$6
 fi
 
-python train.py --dataroot ./datasets/facades --checkpoints_dir --name semifacades_pix2pix_gpu_${GPU_ID}_stream_${NUM_STREAM} --model semipix2pix --which_direction BtoA --display_id -1 --aligned_random_crop 1 --clip_size ${CLIP_SIZE} --gpu_ids ${GPU_ID} --num_stream ${NUM_STREAM} --add_constraint ${MASK_CONSTRAINT} --use_gt_mask ${GT_MASK} --gate_on_gt ${GATE_ON_GT}
+python train.py --dataroot ./datasets/facades --name semifacades_pix2pix_gpu_${GPU_ID}_stream_${NUM_STREAM} --model semipix2pix --which_direction BtoA --display_id -1 --aligned_random_crop 1 --clip_size ${CLIP_SIZE} --gpu_ids ${GPU_ID} --num_stream ${NUM_STREAM} --add_constraint ${MASK_CONSTRAINT} --use_gt_mask ${GT_MASK} --gate_on_gt ${GATE_ON_GT}
