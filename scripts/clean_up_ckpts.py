31a484ebaff98dda2bf6fbf2e47f33a38c2b255b
Yi Shi <yishi.1204@gmai.com>
2021-01-02 00:06:57 -0800

    add scripts to clean up ckpts

---
+++ scripts/clean_up_ckpts.py
@@ -0,0 +1,27 @@
+#
+# run `sudo python scripts/clean_up_ckpts.py /home/yishi/pytorch-CycleGAN-and-pix2pix/checkpoints/ 200`
+#
+
+
+import os
+import sys
+
+root_path = sys.argv[1]
+latest_epoch = sys.argv[2]
+
+def delete_ckpts(current_path):
+    file_names = os.listdir(current_path)
+    ckpt_names = [file_name for file_name in file_names if file_name.endswith(".pth") and not file_name.startswith("latest") and not file_name.startswith(latest_epoch)]
+    if len(ckpt_names) > 0:
+        for ckpt_name_to_delete in ckpt_names:
+            ckpt_path_to_delete = os.path.join(current_path, ckpt_name_to_delete)
+            os.remove(ckpt_path_to_delete)
+        return
+    else:
+        # Go one level lower.
+        for file_name in file_names:
+            path = os.path.join(current_path, file_name)
+            if os.path.isdir(path):
+                delete_ckpts(path)
+
+delete_ckpts(root_path)
